046bd97765a6269d024c359be91c81f36bd339bb
Juan David Gonzalez Piza <jdgonzal2@gmail.com>
2023-05-24 13:24:40 -0500

    feat: qwik. Introducción al Framework [S3/L08] - qwik-city / routeLoader$
    Change the "useLocation" by "routeLoader$"
    
    #20230524a_S3_L08

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,16 +1,28 @@
 import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import { PokemonImage } from "~/components/pokemons/pokemon-image";
 
+export const usePokemonId = routeLoader$<number>(({params, redirect})=>{
+  const id = parseInt(params.id);
+
+  if( isNaN(id) )redirect(301,'/');
+  if(id<=0 || id>=1000) redirect(301,'/');
+
+  return id;
+
+});
+
 export default component$(() => {
-  const location = useLocation();
+  // const location = useLocation();
+
+  const pokemonId = usePokemonId();
 
   return (
     <>
-      <span class="text-5xl">Pokemon: {location.params.id}</span>
+      <span class="text-5xl">Pokemon: {pokemonId}</span>
 
       <PokemonImage
-        id={parseInt(location.params.id)}
+        id={pokemonId.value}
         isVisible={true}
         size={300}
       />
